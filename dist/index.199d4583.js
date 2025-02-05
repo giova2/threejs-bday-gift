efdca29cff4f871b94e6b8b35b888aa3ce4f1c3c
Franco <testing.portfolio.fg@gmail.com>
2025-02-05 11:27:31 -0300

    Deploy static site

---
+++ dist/index.199d4583.js
@@ -0,0 +1,2 @@
+localStorage.getItem("nroMsj")&&parseInt(localStorage.getItem("nroMsj")),document.querySelector("#marquesina"),window.onload=()=>{document.body.querySelector("#detener-marquesina").disabled=!1};
+//# sourceMappingURL=index.199d4583.js.map
