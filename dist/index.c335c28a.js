62fc7051ab990cd765418d065563454f5d600e54
Franco <testing.portfolio.fg@gmail.com>
2025-02-05 12:05:59 -0300

    modified the parcel build command

---
+++ dist/index.c335c28a.js
@@ -0,0 +1,2 @@
+localStorage.getItem("nroMsj")&&parseInt(localStorage.getItem("nroMsj")),document.querySelector("#marquesina"),window.onload=()=>{document.body.querySelector("#detener-marquesina").disabled=!1};
+//# sourceMappingURL=index.c335c28a.js.map
